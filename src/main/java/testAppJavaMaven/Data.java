f067a13ba7b058f3ca76afd96ee9cbb0af0bb3a6
jenkins-push <non_111111@hotmail.com>
2023-04-03 08:10:04 +1000

    2nd order sql injection from function

--- src/main/java/testAppJavaMaven/Data.java
+++ src/main/java/testAppJavaMaven/Data.java
@@ -61,7 +61,7 @@ public class Data extends HttpServlet {
         	out.println("<p><b>Results:</b></p>");
 
 			StringBuilder sql = new StringBuilder("select * from users where userid = '");
-			sql.append(userid); 
+			sql.append(retrieve()); 
 			sql.append("'");
 			PreparedStatement pstmt = connection.prepareStatement( sql.toString() ); 
             ResultSet rs = pstmt.executeQuery(sql.toString());  
@@ -119,4 +119,34 @@ public class Data extends HttpServlet {
 			System.out.println(e);
 		}  		
 	}
+	
+	private String retrieve() {
+		
+		String returnStr = null;
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/insecureapp","insecureapp","45EUlZOpL7");  // #7 Hardcoded password
+			
+			String sql = "select * from users where userid = 'aaa'";
+			PreparedStatement pstmt = connection.prepareStatement( sql); 
+            ResultSet rs = pstmt.executeQuery(sql);  
+            
+            while(rs.next()) {
+            	System.out.println("<p>Name: " + rs.getString(3) + " " + rs.getString(2) + "<br>Address: " + rs.getString(4) + "<br>Phone no: " + rs.getString(5) + "</p>"); // #10 Stored XSS
+            }
+            returnStr = rs.getNString(3);
+            connection.close();
+        } 
+		catch(Exception e){ 
+			System.out.println(e);
+		}  	
+		
+		return returnStr;
+		
+		
+	}
+	
+	
 }
