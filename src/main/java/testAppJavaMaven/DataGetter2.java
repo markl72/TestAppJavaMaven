ed60a96cab1274a9ea58ee74a97c30ed09fc1f3d
jenkins-push <non_111111@hotmail.com>
2023-04-16 17:20:26 +1000

    getset 2 - 2nd order sqli

--- src/main/java/testAppJavaMaven/DataGetter2.java
+++ src/main/java/testAppJavaMaven/DataGetter2.java
@@ -25,7 +25,34 @@ public class DataGetter2 extends HttpServlet {
 	private String getset;
 	
 	public static String getString() {
-		return "user3";		
+
+		String returnStr = null;
+
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/insecureapp","insecureapp","45EUlZOpL7");  // #7 Hardcoded password
+			
+
+			String sql = "select * from users where userid = 'user2'"; //AND password='" + password + "'";
+			PreparedStatement pstmt = connection.prepareStatement(sql); 
+            ResultSet rs = pstmt.executeQuery(sql);  
+            
+             
+            while(rs.next()) {
+            	System.out.println("<p>Name: " + rs.getString(3) + " " + rs.getString(2) + "<br>Address: " + rs.getString(4) + "<br>Phone no: " + rs.getString(5) + "</p>"); // #10 Stored XSS
+            	returnStr = rs.getString(3);
+            }
+            connection.close();
+   
+            
+        } 
+		catch(Exception e){ 
+			System.out.println(e);
+		}  	
+		
+		return returnStr;
+		
 	}
 	
 	public void setString(String getset) {
